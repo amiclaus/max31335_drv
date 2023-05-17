271e1b1fa4e3d57d673b7ebe72932c993e407741
Antoniu Miclaus <antoniu.miclaus@analog.com>
2023-05-17 15:27:29 +0300

    add rtc ops implementation

--- rtc-max31335.c
+++ rtc-max31335.c
@@ -101,12 +101,138 @@ static const struct regmap_config regmap_config = {
 	.max_register = 0x5F,
 };
 
+static int max31335_get_time(struct device *dev, struct rtc_time *tm)
+{
+	struct max31335_data *max31335 = dev_get_drvdata(dev);
+	u8 date[7];
+	int ret;
+
+	ret = regmap_bulk_read(max31335->regmap, MAX31335_SECONDS, date, sizeof(date));
+	if (ret)
+		return ret;
+
+	tm->tm_sec  = bcd2bin(date[0] & 0x7f);
+	tm->tm_min  = bcd2bin(date[1] & 0x7f);
+	tm->tm_hour = bcd2bin(date[2] & 0x3f);
+	tm->tm_wday = date[3] & 0x7;
+	tm->tm_mday = bcd2bin(date[4] & 0x3f);
+	tm->tm_mon  = bcd2bin(date[5] & 0x1f) - 1;
+	tm->tm_year = bcd2bin(date[6]) + 100;
+
+	return 0;
+}
+
+static int max31335_set_time(struct device *dev, struct rtc_time *tm)
+{
+	struct max31335_data *max31335 = dev_get_drvdata(dev);
+	u8 date[7];
+	int ret;
+
+	date[0] = bin2bcd(tm->tm_sec);
+	date[1] = bin2bcd(tm->tm_min);
+	date[2] = bin2bcd(tm->tm_hour);
+	date[3] = tm->tm_wday;
+	date[4] = bin2bcd(tm->tm_mday);
+	date[5] = bin2bcd(tm->tm_mon + 1);
+	date[6] = bin2bcd(tm->tm_year - 100);
+
+	ret = regmap_bulk_write(max31335->regmap, MAX31335_SECONDS, date,
+				sizeof(date));
+	if (ret)
+		return ret;
+}
+
+static int max31335_read_offset(struct device *dev, long *offset)
+{
+	struct max31335_data *max31335 = dev_get_drvdata(dev);
+	int ret, value, steps;
+
+	ret = regmap_read(max31335->regmap, MAX31335_AGING_OFFSET, &value);
+	if (ret)
+		return ret;
+}
+
+static int max31335_set_offset(struct device *dev, long offset)
+{
+	return regmap_write(max31335->regmap, MAX31335_AGING_OFFSET, offset);
+}
+
+static int max31335_get_alarm(struct device *dev, struct rtc_wkalrm *alrm)
+{
+	struct max31335_data *max31335 = dev_get_drvdata(dev);
+	u8 regs[6], ctrl, status;
+	int ret;
+
+	ret = regmap_bulk_read(max31335->regmap, MAX31335_ALM1_SEC, regs, sizeof(regs));
+	if (ret)
+		return ret;
+
+	tm->tm_sec  = bcd2bin(regs[0] & 0x7f);
+	tm->tm_min  = bcd2bin(regs[1] & 0x7f);
+	tm->tm_hour = bcd2bin(regs[2] & 0x3f);
+	tm->tm_mday = bcd2bin(regs[3] & 0x3f);
+	tm->tm_mon  = bcd2bin(regs[4] & 0x1f);
+	tm->tm_year = bcd2bin(regs[5]) + 100;
+
+	ret = regmap_read(max31335->regmap, MAX31335_INT_EN, &ctrl);
+	if (ret)
+		return ret;
+
+	ret = regmap_read(max31335->regmap, MAX31335_STATUS1, &status);
+	if (ret)
+		return ret;
+
+	alrm->enabled = !!(ctrl & MAX31335_INT_EN1_A1IE);
+	alrm->pendind = !!(status & MAX31335_STATUS1_A1F);
+
+	return 0;
+}
+
+static int max31335_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
+{
+	struct max31335_data *max31335 = dev_get_drvdata(dev);
+	u8 regs[6];
+	int ret;
+
+	regs[0] = bin2bcd(tm->tm_sec);
+	regs[1] = bin2bcd(tm->tm_min);
+	regs[2] = bin2bcd(tm->tm_hour);
+	regs[3] = bin2bcd(tm->tm_mday);
+	date[4] = bin2bcd(tm->tm_mon + 1);
+	date[5] = bin2bcd(tm->tm_year - 100);
+
+	ret = regmap_bulk_write(max31335->regmap, MAX31335_ALM1_SEC, regs, sizeof(regs));
+	if (ret)
+		return ret;
+
+	ret = regmap_update_bits(max31335->regmap, MAX31335_STATUS1,
+				 MAX31335_STATUS1_A1F, 0);
+	if (ret)
+		return ret;
+
+	return regmap_update_bits(max31335->regmap, MAX31335_INT_EN,
+				  MAX31335_INT_EN1_A1IE, MAX31335_INT_EN1_A1IE);
+}
+
+static int max31335_alarm_irq_enable(struct device *dev, unsigned int enabled)
+{
+	struct max31335_data *max31335 = dev_get_drvdata(dev);
+	int ret;
+
+	ret = regmap_update_bits(max31335->regmap, MAX31335_STATUS1,
+				 MAX31335_STATUS1_A1F, 0);
+	if (ret)
+		return ret;
+
+	return regmap_update_bits(max31335->regmap, MAX31335_INT_EN,
+				  MAX31335_INT_EN1_A1IE, MAX31335_INT_EN1_A1IE);
+}
+
 static const struct rtc_class_ops max31335_rtc_ops = {
 	.read_time = max31335_get_time,
 	.set_time = max31335_set_time,
 	.read_offset = max31335_read_offset,
 	.set_offset = max31335_set_offset,
-	.ioctl = max31335_ioctl,
 	.read_alarm = max31335_get_alarm,
 	.set_alarm = max31335_set_alarm,
 	.alarm_irq_enable = max31335_alarm_irq_enable,
