1354177b556eaf429d5573d7dfeda9684031eb61
Antoniu Miclaus <antoniu.miclaus@analog.com>
2023-06-28 15:48:09 +0300

    update register definition

--- rtc-max31335.c
+++ rtc-max31335.c
@@ -24,92 +24,164 @@
 #include <linux/rtc.h>
 #include <linux/util_macros.h>
 
-#define MAX31335_STATUS1		0x00
-#define MAX31335_INT_EN			0x01
-#define MAX31335_STATUS2		0x02
-#define MAX31335_INT_EN2		0x03
-#define MAX31335_RTC_RESET		0x04
-#define MAX31335_RTC_CONFIG		0x05
-#define MAX31335_RTC_CONFIG2		0x06
-#define MAX31335_TIMESTAMP_CONFIG	0x07
-#define MAX31335_TIMER_CONFIG		0x08
-#define MAX31335_SECONDS_1_128		0x09
-#define MAX31335_SECONDS		0x0A
-#define MAX31335_MINUTES		0x0B
-#define MAX31335_HOURS			0x0C
-#define MAX31335_DAY			0x0D
-#define MAX31335_DATE			0x0E
-#define MAX31335_MONTH			0x0F
-#define MAX31335_YEAR			0x0F
-#define MAX31335_ALM1_SEC		0x11
-#define MAX31335_ALM1_MIN		0x12
-#define MAX31335_ALM1_HRS		0x13
-#define MAX31335_ALM1_DAY_DATE		0x14
-#define MAX31335_ALM1_MON		0x15
-#define MAX31335_ALM1_YEAR		0x16
-#define MAX31335_ALM2_MIN		0x17
-#define MAX31335_ALM2_HRS		0x18
-#define MAX31335_ALM2_DAY_DATE		0x19
-#define MAX31335_TIMER_COUNT		0x1A
-#define MAX31335_TIMER_INIT		0x1B
-#define MAX31335_PWR_MGMT		0x1C
-#define MAX31335_TRICKLE_REG		0x1D
-#define MAX31335_AGING_OFFSET		0x1E
-#define MAX31335_TS_CONFIG		0x30
-#define MAX31335_TEMP_ALARM_HIGH_MSB	0x31
-#define MAX31335_TEMP_ALARM_HIGH_LSB	0x32
-#define MAX31335_TEMP_ALARM_LOW_MSB	0x33
-#define MAX31335_TEMP_ALARM_LOW_LSB	0x34
-#define MAX31335_TEMP_DATA_MSB		0x35
-#define MAX31335_TEMP_DATA_LSB		0x36
-#define MAX31335_TS0_SEC_1_128		0x40
-#define MAX31335_TS0_SEC		0x41
-#define MAX31335_TS0_MIN		0x42
-#define MAX31335_TS0_HOUR		0x43
-#define MAX31335_TS0_DATE		0x44
-#define MAX31335_TS0_MONTH		0x45
-#define MAX31335_TS0_YEAR		0x46
-#define MAX31335_TS0_FLAGS		0x47
-#define MAX31335_TS1_SEC_1_128		0x48
-#define MAX31335_TS1_SEC		0x49
-#define MAX31335_TS1_MIN		0x4A
-#define MAX31335_TS1_HOUR		0x4B
-#define MAX31335_TS1_DATE		0x4C
-#define MAX31335_TS1_MONTH		0x4D
-#define MAX31335_TS1_YEAR		0x4E
-#define MAX31335_TS1_FLAGS		0x4F
-#define MAX31335_TS2_SEC_1_128		0x50
-#define MAX31335_TS2_SEC		0x51
-#define MAX31335_TS2_MIN		0x52
-#define MAX31335_TS2_HOUR		0x53
-#define MAX31335_TS2_DATE		0x54
-#define MAX31335_TS2_MONTH		0x55
-#define MAX31335_TS2_YEAR		0x56
-#define MAX31335_TS2_FLAGS		0x57
-#define MAX31335_TS3_SEC_1_128		0x58
-#define MAX31335_TS3_SEC		0x59
-#define MAX31335_TS3_MIN		0x5A
-#define MAX31335_TS3_HOUR		0x5B
-#define MAX31335_TS3_DATE		0x5C
-#define MAX31335_TS3_MONTH		0x5D
-#define MAX31335_TS3_YEAR		0x5E
-#define MAX31335_TS3_FLAGS		0x5F
+/* MAX31335 Register Map */
+#define MAX31335_STATUS1			0x00
+#define MAX31335_INT_EN1			0x01
+#define MAX31335_STATUS2			0x02
+#define MAX31335_INT_EN2			0x03
+#define MAX31335_RTC_RESET			0x04
+#define MAX31335_RTC_CONFIG			0x05
+#define MAX31335_RTC_CONFIG2			0x06
+#define MAX31335_TIMESTAMP_CONFIG		0x07
+#define MAX31335_TIMER_CONFIG			0x08
+#define MAX31335_SECONDS_1_128			0x09
+#define MAX31335_SECONDS			0x0A
+#define MAX31335_MINUTES			0x0B
+#define MAX31335_HOURS				0x0C
+#define MAX31335_DAY				0x0D
+#define MAX31335_DATE				0x0E
+#define MAX31335_MONTH				0x0F
+#define MAX31335_YEAR				0x0F
+#define MAX31335_ALM1_SEC			0x11
+#define MAX31335_ALM1_MIN			0x12
+#define MAX31335_ALM1_HRS			0x13
+#define MAX31335_ALM1_DAY_DATE			0x14
+#define MAX31335_ALM1_MON			0x15
+#define MAX31335_ALM1_YEAR			0x16
+#define MAX31335_ALM2_MIN			0x17
+#define MAX31335_ALM2_HRS			0x18
+#define MAX31335_ALM2_DAY_DATE			0x19
+#define MAX31335_TIMER_COUNT			0x1A
+#define MAX31335_TIMER_INIT			0x1B
+#define MAX31335_PWR_MGMT			0x1C
+#define MAX31335_TRICKLE_REG			0x1D
+#define MAX31335_AGING_OFFSET			0x1E
+#define MAX31335_TS_CONFIG			0x30
+#define MAX31335_TEMP_ALARM_HIGH_MSB		0x31
+#define MAX31335_TEMP_ALARM_HIGH_LSB		0x32
+#define MAX31335_TEMP_ALARM_LOW_MSB		0x33
+#define MAX31335_TEMP_ALARM_LOW_LSB		0x34
+#define MAX31335_TEMP_DATA_MSB			0x35
+#define MAX31335_TEMP_DATA_LSB			0x36
+#define MAX31335_TS0_SEC_1_128			0x40
+#define MAX31335_TS0_SEC			0x41
+#define MAX31335_TS0_MIN			0x42
+#define MAX31335_TS0_HOUR			0x43
+#define MAX31335_TS0_DATE			0x44
+#define MAX31335_TS0_MONTH			0x45
+#define MAX31335_TS0_YEAR			0x46
+#define MAX31335_TS0_FLAGS			0x47
+#define MAX31335_TS1_SEC_1_128			0x48
+#define MAX31335_TS1_SEC			0x49
+#define MAX31335_TS1_MIN			0x4A
+#define MAX31335_TS1_HOUR			0x4B
+#define MAX31335_TS1_DATE			0x4C
+#define MAX31335_TS1_MONTH			0x4D
+#define MAX31335_TS1_YEAR			0x4E
+#define MAX31335_TS1_FLAGS			0x4F
+#define MAX31335_TS2_SEC_1_128			0x50
+#define MAX31335_TS2_SEC			0x51
+#define MAX31335_TS2_MIN			0x52
+#define MAX31335_TS2_HOUR			0x53
+#define MAX31335_TS2_DATE			0x54
+#define MAX31335_TS2_MONTH			0x55
+#define MAX31335_TS2_YEAR			0x56
+#define MAX31335_TS2_FLAGS			0x57
+#define MAX31335_TS3_SEC_1_128			0x58
+#define MAX31335_TS3_SEC			0x59
+#define MAX31335_TS3_MIN			0x5A
+#define MAX31335_TS3_HOUR			0x5B
+#define MAX31335_TS3_DATE			0x5C
+#define MAX31335_TS3_MONTH			0x5D
+#define MAX31335_TS3_YEAR			0x5E
+#define MAX31335_TS3_FLAGS			0x5F
 
-#define MAX31335_STATUS1_A1F		BIT(0)
-#define MAX31335_INT_EN1_A1IE		BIT(0)
+/* MAX31335_STATUS1 Bit Definitions */
+#define MAX31335_STATUS1_PSDECT			BIT(7)
+#define MAX31335_STATUS1_OSF			BIT(6)
+#define MAX31335_STATUS1_PFAIL			BIT(5)
+#define MAX31335_STATUS1_VBATLOW		BIT(4)
+#define MAX31335_STATUS1_DIF			BIT(3)
+#define MAX31335_STATUS1_TIF			BIT(2)
+#define MAX31335_STATUS1_A2F			BIT(1)
+#define MAX31335_STATUS1_A1F			BIT(0)
 
-#define MAX31335_TRICKLE		GENMASK(3, 1)
-#define MAX31335_EN_TRICKLE		BIT(0)
+/* MAX31335_INT_EN1 Bit Definitions */
+#define MAX31335_INT_EN1_DOSF			BIT(6)
+#define MAX31335_INT_EN1_PFAILE			BIT(5)
+#define MAX31335_INT_EN1_VBATLOWE		BIT(4)
+#define MAX31335_INT_EN1_DIE			BIT(3)
+#define MAX31335_INT_EN1_TIE			BIT(2)
+#define MAX31335_INT_EN1_A2IE			BIT(1)
+#define MAX31335_INT_EN1_A1IE			BIT(0)
 
-#define MAX31335_HRS_F_AM_PM		BIT(5)
-#define MAX31335_HRS_F_12_24		BIT(6)
-#define MAX31335_MONTH_CENTURY		BIT(7)
+/* MAX31335_STATUS2 Bit Definitions */
+#define MAX31335_STATUS2_TEMP_RDY		BIT(2)
+#define MAX31335_STATUS2_OTF			BIT(1)
+#define MAX31335_STATUS2_UTF			BIT(0)
 
-#define MAX31335_ENCLKO			BIT(2)
+/* MAX31335_INT_EN2 Bit Definitions */
+#define MAX31335_INT_EN2_TEMP_RDY_EN		BIT(2)
+#define MAX31335_INT_EN2_OTIE			BIT(1)
+#define MAX31335_INT_EN2_UTIE			BIT(0)
 
-#define MAX31335_STATUS1_DEFAULT	0x40
-#define MAX31335_RAM_SIZE		64
-#define MAX31335_TIME_SIZE		0x07
+/* MAX31335_RTC_RESET Bit Definitions */
+#define MAX31335_RTC_RESET_SWRST		BIT(0)
+
+/* MAX31335_RTC_CONFIG1 Bit Definitions */
+#define MAX31335_RTC_CONFIG1_EN_IO		BIT(6)
+#define MAX31335_RTC_CONFIG1_A1AC		GENMASK(5, 4)
+#define MAX31335_RTC_CONFIG1_DIP		BIT(3)
+#define MAX31335_RTC_CONFIG1_I2C_TIMEOUT	BIT(1)
+#define MAX31335_RTC_CONFIG1_EN_OSC		BIT(0)
+
+/* MAX31335_RTC_CONFIG2 Bit Definitions */
+#define MAX31335_RTC_CONFIG2_ENCLKO		BIT(2)
+#define MAX31335_RTC_CONFIG2_CLKO_HZ		GENMASK(1, 0)
+
+/* MAX31335_TIMESTAMP_CONFIG Bit Definitions */
+#define MAX31335_TIMESTAMP_CONFIG_TSVLOW	BIT(5)
+#define MAX31335_TIMESTAMP_CONFIG_TSPWM		BIT(4)
+#define MAX31335_TIMESTAMP_CONFIG_TSDIN		BIT(3)
+#define MAX31335_TIMESTAMP_CONFIG_TSOW		BIT(2)
+#define MAX31335_TIMESTAMP_CONFIG_TSR		BIT(1)
+#define MAX31335_TIMESTAMP_CONFIG_TSE		BIT(0)
+
+/* MAX31335_TIMER_CONFIG Bit Definitions */
+#define MAX31335_TIMER_CONFIG_TE		BIT(4)
+#define MAX31335_TIMER_CONFIG_TPAUSE		BIT(3)
+#define MAX31335_TIMER_CONFIG_TRPT		BIT(2)
+#define MAX31335_TIMER_CONFIG_TFS		GENMASK(1, 0)
+
+/* MAX31335_HOURS Bit Definitions */
+#define MAX31335_HOURS_F_24_12			BIT(6)
+#define MAX31335_HOURS_HR_20_AM_PM		BIT(5)
+
+/* MAX31335_MONTH Bit Definitions */
+#define MAX31335_MONTH_CENTURY			BIT(7)
+
+/* MAX31335_PWR_MGMT Bit Definitions */
+#define MAX31335_PWR_MGMT_PFVT			BIT(0)
+
+/* MAX31335_TRICKLE_REG Bit Definitions */
+#define MAX31335_TRICKLE_REG_TRICKLE		GENMASK(3, 1)
+#define MAX31335_TRICKLE_REG_EN_TRICKLE		BIT(0)
+
+/* MAX31335_TS_CONFIG Bit Definitions */
+#define MAX31335_TS_CONFIG_AUTO			BIT(4)
+#define MAX31335_TS_CONFIG_CONVERT_T		BIT(3)
+#define MAX31335_TS_CONFIG_TSINT		GENMASK(2, 0)
+
+/* MAX31335_TS_FLAGS Bit Definitions */
+#define MAX31335_TS_FLAGS_VLOWF			BIT(3)
+#define MAX31335_TS_FLAGS_VBATF			BIT(2)
+#define MAX31335_TS_FLAGS_VCCF			BIT(1)
+#define MAX31335_TS_FLAGS_DINF			BIT(0)
+
+/* MAX31335 Miscellaneous Definitions */
+#define MAX31335_STATUS1_DEFAULT		0x40
+#define MAX31335_RAM_SIZE			64
+#define MAX31335_TIME_SIZE			0x07
 
 #define clk_hw_to_max31335(_hw) container_of(_hw, struct max31335_data, clkout)
 
@@ -150,7 +222,7 @@ static int max31335_get_hour(u8 hour_reg)
 	int hour;
 
 	/* 24Hr mode */
-	if (!FIELD_GET(MAX31335_HRS_F_12_24, hour_reg))
+	if (!FIELD_GET(MAX31335_HOURS_F_24_12, hour_reg))
 		return bcd2bin(hour_reg & 0x3f);
 
 	/* 12Hr mode */
@@ -158,7 +230,7 @@ static int max31335_get_hour(u8 hour_reg)
 	if (hour == 12)
 		hour = 0;
 
-	if (FIELD_GET(MAX31335_HRS_F_AM_PM, hour_reg))
+	if (FIELD_GET(MAX31335_HOURS_HR_20_AM_PM, hour_reg))
 		hour += 12;
 
 	return hour;
@@ -256,7 +328,7 @@ static int max31335_read_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 	if (time.tm_year >= 200)
 		alrm->time.tm_year += 100;
 
-	ret = regmap_read(max31335->regmap, MAX31335_INT_EN, &ctrl);
+	ret = regmap_read(max31335->regmap, MAX31335_INT_EN1, &ctrl);
 	if (ret)
 		return ret;
 
@@ -290,7 +362,7 @@ static int max31335_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 		return ret;
 
 	reg = FIELD_PREP(MAX31335_INT_EN1_A1IE, alrm->enabled);
-	return regmap_update_bits(max31335->regmap, MAX31335_INT_EN,
+	return regmap_update_bits(max31335->regmap, MAX31335_INT_EN1,
 				  MAX31335_INT_EN1_A1IE, reg);
 	if (ret)
 		return ret;
@@ -303,7 +375,7 @@ static int max31335_alarm_irq_enable(struct device *dev, unsigned int enabled)
 {
 	struct max31335_data *max31335 = dev_get_drvdata(dev);
 
-	return regmap_update_bits(max31335->regmap, MAX31335_INT_EN,
+	return regmap_update_bits(max31335->regmap, MAX31335_INT_EN1,
 				  MAX31335_INT_EN1_A1IE, enabled);
 }
 
@@ -372,7 +444,7 @@ static int max31335_trickle_charger_setup(struct device *dev, struct max31335_da
 		i = i + 1;
 
 	return regmap_write(max31335->regmap, MAX31335_TRICKLE_REG,
-			    FIELD_PREP(MAX31335_TRICKLE, i) | MAX31335_EN_TRICKLE);
+			    FIELD_PREP(MAX31335_TRICKLE_REG_TRICKLE, i) | MAX31335_TRICKLE_REG_EN_TRICKLE);
 }
 
 static unsigned long max31335_clkout_recalc_rate(struct clk_hw *hw,
@@ -421,14 +493,14 @@ static int max31335_clkout_enable(struct clk_hw *hw)
 {
 	struct max31335_data *max31335 = clk_hw_to_max31335(hw);
 
-	return regmap_set_bits(max31335->regmap, MAX31335_RTC_CONFIG2, MAX31335_ENCLKO);
+	return regmap_set_bits(max31335->regmap, MAX31335_RTC_CONFIG2, MAX31335_RTC_CONFIG2_ENCLKO);
 }
 
 static void max31335_clkout_disable(struct clk_hw *hw)
 {
 	struct max31335_data *max31335 = clk_hw_to_max31335(hw);
 
-	regmap_clear_bits(max31335->regmap, MAX31335_RTC_CONFIG2, MAX31335_ENCLKO);
+	regmap_clear_bits(max31335->regmap, MAX31335_RTC_CONFIG2, MAX31335_RTC_CONFIG2_ENCLKO);
 }
 
 static int max31335_clkout_is_enabled(struct clk_hw *hw)
@@ -441,7 +513,7 @@ static int max31335_clkout_is_enabled(struct clk_hw *hw)
 	if (ret)
 		return ret;
 
-	return !!(reg & MAX31335_ENCLKO);
+	return !!(reg & MAX31335_RTC_CONFIG2_ENCLKO);
 }
 
 static const struct clk_ops max31335_clkout_ops = {
